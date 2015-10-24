a0f4958325967c554f0a603e86758fbcf1b67bf4
antonioorodungao <antoniodungao@yahoo.com>
2015-10-25 02:30:08 +0300

    Lesson 67: Changing Font

--- src/gui/TextPanel.java
+++ src/gui/TextPanel.java
@@ -13,6 +13,8 @@ public class TextPanel extends JPanel {
         textArea = new JTextArea();
         textArea.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
 
+        textArea.setFont(new Font(Font.SERIF, Font.PLAIN, 20));
+
         setLayout(new BorderLayout());
         add(new JScrollPane(textArea), BorderLayout.CENTER);
 
